1f80efc7836f1208bb6fcbfd214ca52d5c4174a0
Catherine Jones <cejones75@gmail.com>
2016-05-31 22:09:25 +0100

    created function doLEDs

--- NodeJs/LEDsTest.js
+++ NodeJs/LEDsTest.js
@@ -6,22 +6,30 @@ var pixelData = new Uint32Array(NUM_LEDS);
 
 var brightness = 128;
 
-ws281x.init(NUM_LEDS);
 
-ws281x.setBrightness(brightness);
 
-ws281x.reset();
+function doLEDs(){
 
-for(var i = 0; i < NUM_LEDS; i++){
-	pixelData[i] = (getRandomInt(0,255), getRandomInt(0,255), getRandomInt(0,255));
-	ws281x.render(pixelData);
+	ws281x.init(NUM_LEDS);
+
+    ws281x.setBrightness(brightness);
+
+    ws281x.reset();
+
+    for(var i = 0; i < NUM_LEDS; i++){
+	    pixelData[i] = (getRandomInt(0,255), getRandomInt(0,255), getRandomInt(0,255));
+	    ws281x.render(pixelData);
+
+    }
 
 }
-
 
 
 function getRandomInt(min, max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
+
+doLEDs();
+
 
